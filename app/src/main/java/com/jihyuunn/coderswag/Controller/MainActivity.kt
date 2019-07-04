da4198f307c8a8e84d531d38e82bde79e4887ba9
jihyuunn <791004_@likelion.org>
2019-07-04 22:24:25 +0900

    make list layout

--- app/src/main/java/com/jihyuunn/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/jihyuunn/coderswag/Controller/MainActivity.kt
@@ -2,12 +2,24 @@ package com.jihyuunn.coderswag.Controller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.jihyuunn.coderswag.Model.Category
 import com.jihyuunn.coderswag.R
+import com.jihyuunn.coderswag.Service.DataService
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var adapter : ArrayAdapter<Category>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        adapter = ArrayAdapter(this,
+            android.R.layout.simple_list_item_1,
+            DataService.categories)
+
+        categoryListView.adapter = adapter
     }
 }
